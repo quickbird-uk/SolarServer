8d3cb6354b2318f956c75eb4f2b62cb1cc1f00fb
Vladimir Akopyan <Vladimir@akopyan.me>
2016-12-29 13:02:14 +0000

    FirstTest

---
+++ src/WebApiTest/Formatters/BinaryInputFormatter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace WebApiTest
+{
+    public class BinaryInputFormatter : IInputFormatter
+    {
+        public bool CanRead(InputFormatterContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            var contentType = context.HttpContext.Request.ContentType;
+            if (contentType == null
+                || contentType == "quickbird/BinaryArch2v1")
+                return true;
+            return false;
+        }
+
+        public Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            var request = context.HttpContext.Request;
+            if (request.ContentLength == 0)
+            {
+                if (context.ModelType.GetTypeInfo().IsValueType)
+                    return InputFormatterResult.SuccessAsync(Activator.CreateInstance(context.ModelType));
+                else return InputFormatterResult.SuccessAsync(null);
+            }
+
+           
+            var stream = context.HttpContext.Request.Body;
+
+            return ReadAndDecode(stream);
+        }
+
+        public async Task<InputFormatterResult> ReadAndDecode(Stream stream)
+        {
+            List<Model.Datapoint> list = new List<Model.Datapoint>();
+
+            int buffersize = 10000;
+            byte[] buffer = new byte[buffersize];
+            int bufferFilled = 0;
+            int bytesRead = 0;
+
+            //Read al lteh data
+            do
+            {
+                bytesRead = await stream.ReadAsync(buffer, bufferFilled, buffersize - bufferFilled);
+                bufferFilled += bytesRead;
+            } while (bytesRead > 0);
+
+            int i;
+
+            List<string> sensorsInMessage = new List<string>(); 
+
+            //Read Sensor Header
+            for(i = 0; i < bufferFilled; i+=2)
+            {
+                short sensorID = BitConverter.ToInt16(buffer, i);
+
+                if (sensorID == 0)
+                    break;
+
+                var sensor = Model.Datapoint.SensorIdLUT.FirstOrDefault(); //TODO: error check
+                sensorsInMessage.Add(sensor.Value); 
+            }
+
+            while (i < bufferFilled)
+            {
+                Model.Datapoint datapoint = new Model.Datapoint();
+
+                int unixEpoc = BitConverter.ToInt32(buffer, i);
+                datapoint.UTCTimestamp = DateTimeOffset.FromUnixTimeSeconds(unixEpoc).UtcDateTime;
+                i += 4; //move along by 4 because we read an int; 
+
+                datapoint.chargeState = (Model.ChargeState) buffer[i]; //We are using the same numbers to determine this, so we can just convert directly
+
+                datapoint.BatteryVoltage = BitConverter.ToSingle(buffer, i);
+                i += 4; 
+
+                for(int s = 0; s < sensorsInMessage.Count; s++)
+                {
+                    string key = sensorsInMessage[s];
+                    float value = BitConverter.ToSingle(buffer, i);
+                    datapoint.sensorReadings.Add(key, value);
+                    i += 4;
+                }
+
+            }
+
+            return InputFormatterResult.Success(list); 
+        }
+
+
+    }
+}
---
+++ src/WebApiTest/Model/Datapoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Model
+{
+    public enum ChargeState
+    {
+        Invalid, Charging, ChargeDone, NoCharge
+    }
+    public class Datapoint
+    {
+        public DateTime UTCTimestamp;
+
+        public ChargeState chargeState;
+
+        public float BatteryVoltage;
+
+        public Dictionary<string, float> sensorReadings = new Dictionary<string, float>();
+
+        public static Dictionary<short, string> SensorIdLUT = new Dictionary<short, string>()
+        {
+            { 4, "Light - outdoors"},
+            { 5, "Pressure - Internal"},
+            { 6, "Humidity - Internal"},
+            { 7, "Air Temperature - Internal" },
+            { 8, "Air Temperature - External" },
+            { 9, "Humidity - External" },
+            { 10, "Pressure - External" },
+            { 11, "Soil Temperature 1" },
+            { 12, "Soil Temperature 2" },
+            { 13, "Soil Temperature 3" },
+            { 13, "Soil Moisture 1" },
+            { 14, "Soil Moisture 2" },
+            { 15, "Soil Moisture 3" },
+         };
+
+    }
+}
