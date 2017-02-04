5e225eb340e16206d4ecf19466fd815d0215440b
Vladimir <vlad@quickbird.uk>
2017-02-04 23:04:57 +0000

    Returning a list of datapoint in correct format

---
+++ src/WebApiTest/Controllers/ReadingsToGraph.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using MongoDB.Bson;
+using Microsoft.AspNetCore.Mvc;
+
+using WebApiTest.Model;
+using System.Dynamic;
+using System.Diagnostics;
+
+namespace WebApiTest.Controllers
+{
+    [Route("api/readings_to_graph")]
+    public class ReadingsToGraphController : Controller
+    {
+        DataAccess objds;
+
+        public ReadingsToGraphController()
+        {
+            objds = new DataAccess();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<List<ChartSeries>> Get(int id)
+        {
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            List<BsonDocument> dbList = await objds.GetDatapointsOfNode(id);
+            List<ChartSeries> returnlist = new List<ChartSeries>();
+            watch.Stop();
+
+            foreach (var doc in dbList)
+            {
+
+                var dateTime = doc.Elements.FirstOrDefault(el => el.Name == "UTCTimestamp")
+                .Value
+                .ToUniversalTime();
+
+                long utcTimestamp = new DateTimeOffset(dateTime, TimeSpan.Zero).ToUnixTimeMilliseconds();
+
+                foreach (var el in doc.Elements)
+                {
+                    //Items we don;t want to return
+                    if (el.Name != "NodeId"
+                        && el.Name != "_id"
+                        && el.Name != "UploadTime"
+                        && el.Name != "UTCTimestamp")
+                    {
+                        var collection = returnlist.FirstOrDefault(rl => rl.key == el.Name);
+                        if (collection == null)
+                        {
+                            collection = new ChartSeries
+                            {
+                                key = el.Name,
+                                values = new List<chartPoint>(dbList.Count)
+                            };
+                            returnlist.Add(collection);
+                        }
+
+                        chartPoint point = new chartPoint
+                        {
+                            x = utcTimestamp
+                        };
+
+
+                        if (el.Value.IsInt32)
+                            point.y = el.Value.AsInt32;
+                        else if (el.Value.IsDouble)
+                            point.y = (float) el.Value.ToDouble();
+
+                        collection.values.Add(point);
+                    }
+                }
+            }
+
+            return returnlist;
+        }
+    }
+
+    public class ChartSeries{
+        public string key;
+        public List<chartPoint> values; 
+    }
+
+    public class chartPoint
+    {
+        public long x;
+        public float y; 
+    }
+}
+
+
